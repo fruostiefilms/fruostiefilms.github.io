e002e24504eab24796f7959f367277e535bad670
FruostieDev <159085497+FruostieDev@users.noreply.github.com>
2024-02-26 20:03:57 +1000

    update website

--- script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const darkModeToggle = document.getElementById('darkModeToggle');
-    const body = document.body;
-
-    darkModeToggle.addEventListener('click', function () {
-        body.classList.toggle('dark-mode');
-    });
-});
